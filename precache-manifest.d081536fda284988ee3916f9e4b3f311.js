d792c7d61a7548cf80ce3171b34d7e5d14bf6eaf
Roberth <rohuavis@gmail.com>
2020-09-04 21:41:23 -0400

    Updates

---
+++ precache-manifest.d081536fda284988ee3916f9e4b3f311.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb125a2953e8998b70b0d225f7a87683",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a31591b2786c6e63a23b",
+    "url": "/monsters-rolodex/static/css/main.5fe25a3e.chunk.css"
+  },
+  {
+    "revision": "efdaa8f6541fe00250cb",
+    "url": "/monsters-rolodex/static/js/2.dbc8e97e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.dbc8e97e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a31591b2786c6e63a23b",
+    "url": "/monsters-rolodex/static/js/main.0c2f0c27.chunk.js"
+  },
+  {
+    "revision": "e01b263a933f37eb7c2d",
+    "url": "/monsters-rolodex/static/js/runtime-main.6136c16c.js"
+  }
+]);
